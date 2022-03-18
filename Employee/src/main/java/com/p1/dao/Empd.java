0a730445958505e2fa52512834eb8db3711f7a9e
Venkatesh <Venkatesh@DESKTOP-PP6TV26>
2022-03-18 09:22:50 +0530

    addition DAO

---
+++ Employee/src/main/java/com/p1/dao/Empd.java
@@ -0,0 +1,15 @@
+package com.p1.dao;
+
+import com.p1.Emp;
+
+public class Empd {
+
+	public static void main(String[] args) {
+		Emp n = new Emp();
+		
+		System.out.println("Welcome");
+		System.out.println(n);
+
+	}
+
+}
